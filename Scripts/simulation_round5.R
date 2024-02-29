6ec755c40011de83effbe3ea56ec9538d95c5790
Julia Pilowsky <japilo@users.noreply.github.com>
2024-02-28 21:11:11 -0500

    Code for simulation round 5

---
+++ Scripts/simulation_round5.R
@@ -0,0 +1,25 @@
+library(poems)
+library(tidyverse)
+library(furrr)
+library(Rcpp)
+library(qs)
+source("Scripts/simulation_functions.R")
+options(future.globals.maxSize= 9967869120)
+synbias_lhs <- LatinHypercubeSampler$new()
+synbias_lhs$set_uniform_parameter("interaction_strength", 0, 0.75)
+synbias_lhs$set_class_parameter("priority_effects", c(TRUE, FALSE))
+synbias_lhs$set_uniform_parameter("cf_ratio")
+synbias_lhs$set_uniform_parameter("strains", 10, 200, decimals = 0)
+sample_data3 <- synbias_lhs$generate_samples(10000, random_seed = 216) %>%
+  rowid_to_column("id")
+plan(sequential)
+inputs <- sample_data3 %>% rowwise() %>% group_split() %>%
+  map(df_to_inputs)
+future_walk(1:length(inputs), \(x) {
+  filename <- paste0("Data/simulation_round5/sim", sample_data3$id[x], ".qs")
+  if (!file.exists(filename))
+    sourceCpp("Scripts/ferrari.cpp")
+    x <- inputs[[x]]
+    mat <- ferrari(x$initial_pop, x$interactions, 2e-04, 100)
+    qsave(mat, filename)
+  }, .progress = T)

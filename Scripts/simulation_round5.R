ce81ba905eee3f464e4bd214fc7046e7eaa8aacb
July Pilowsky <japilo@users.noreply.github.com>
2024-03-04 16:02:42 -0500

    Code for measuring 50% prevalence points

--- Scripts/simulation_round5.R
+++ Scripts/simulation_round5.R
@@ -1,3 +1,4 @@
+#### Set up ####
 library(poems)
 library(tidyverse)
 library(furrr)
@@ -5,6 +6,8 @@ library(Rcpp)
 library(qs)
 source("Scripts/simulation_functions.R")
 options(future.globals.maxSize= 9967869120)
+data_dir <- "Data/simulation_round5/"
+#### Simulation ####
 synbias_lhs <- LatinHypercubeSampler$new()
 synbias_lhs$set_uniform_parameter("interaction_strength", 0, 0.75)
 synbias_lhs$set_class_parameter("priority_effects", c(TRUE, FALSE))
@@ -16,10 +19,25 @@ plan(sequential)
 inputs <- sample_data3 %>% rowwise() %>% group_split() %>%
   map(df_to_inputs)
 future_walk(1:length(inputs), \(x) {
-  filename <- paste0("Data/simulation_round5/sim", sample_data3$id[x], ".qs")
+  filename <- paste0(data_dir, "sim/", sample_data3$id[x], ".qs")
   if (!file.exists(filename))
     sourceCpp("Scripts/ferrari.cpp")
     x <- inputs[[x]]
     mat <- ferrari(x$initial_pop, x$interactions, 2e-04, 100)
     qsave(mat, filename)
   }, .progress = T)
+#### 50 percent prevalence points ####
+# Try it with one example first
+sim1 <- data_dir |> paste0("sim1.qs") |> qread()
+sim1 |> apply(c(2, 3), sum) |> array_branch(1) |> map(\(x) x >= 500) |>
+  map_int(match, x = TRUE)
+# Scale it up
+prevalence50 <- function(path) {
+  path |> qread() |>
+    apply(c(2, 3), sum) |>
+    array_branch(1) |>
+    map(\(x) x >= 500) |>
+    map_int(match, x = TRUE)
+}
+timepoints <- 1:10000 |> map(\(x) paste0(data_dir, "sim/", x, ".qs")) |>
+  map(prevalence50)

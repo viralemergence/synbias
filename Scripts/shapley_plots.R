ac9eb04ea8fe2532813a4a008504b7f5392a9d5f
July Pilowsky <japilo@users.noreply.github.com>
2025-08-04 10:24:19 -0400

    Results from simulation round 7

---
+++ Scripts/shapley_plots.R
@@ -0,0 +1,138 @@
+#' This function creates a set of partial dependence plots
+#' @param variables Vector of the variables to be plotted (if log transformed,
+#' they need to have "log_" added to the front). They should be of length 16.
+#' @param shapley_data These are the Shapley values produced from the
+#' bootstrapping process. They do not need to be processed, but it should be a
+#'  data.frame with columns for: variable (variable name), value (Shapley
+#' value), rfvalue (training data value), stdfvalue (standardized training
+#'  data value from 0 to 1), mean_value (mean Shapley value), ID
+#' (bootstrap ID), row_index (row of training data associated with this
+#' value)
+#' @param train_data The data.frame used to train these models (used to grab
+#' the distribution of values for each variable). The column names should
+#' match what is seen in the variables vector as that is what is used to
+#' grab the values.
+#' @param colors_vec A vector of color values that should be equal in length
+#'  to the number of variables given
+#' @return Returns a partial dependence plot object that is actually 33
+#' panels (16 panels of partial dependence plots, 16 panels of the density
+#'  curves showing the distribution of values in the training data, panel of
+#'  enlarged y-axis title from {cowplot})
+#' @example partial.dependence.plot(CHC, SHAP, DATA, p_cols)
+partial.dependence.plot <- function(
+  variables,
+  shapley_data,
+  train_data,
+  colors_vec
+) {
+  num_variables <- length(variables)
+  if (num_variables != length(colors_vec)) {
+    stop("The number of variables must match the number of colors provided.")
+  }
+  if (num_variables < 1) {
+    stop("There must be at least one variable to plot.")
+  }
+  if (!is.data.frame(shapley_data)) {
+    stop("shapley_data must be a data.frame.")
+  }
+  if (
+    !all(
+      c("variable", "value", "rfvalue", "ID") %in% colnames(shapley_data)
+    )
+  ) {
+    stop(
+      "shapley_data must contain the columns: variable, value, rfvalue, and ID."
+    )
+  }
+  if (!all(variables %in% colnames(train_data))) {
+    stop("All variables must be present in the train_data.")
+  }
+  MNVL <- shapley_data %>%
+    filter(!is.na(rfvalue)) %>%
+    group_by(variable, ID, rfvalue) %>%
+    summarise(
+      value = mean(value, na.rm = T),
+      rfvalue = mean(rfvalue, na.rm = T)
+    ) %>%
+    filter(variable %in% variables) %>%
+    ungroup() %>%
+    data.frame()
+  PDPR <- shapley_data %>%
+    filter(variable %in% variables)
+  PDPR %>%
+    filter(variable %in% variables) %>%
+    group_by(variable) %>%
+    summarise(min = min(value), max = max(value))
+  PDP <- lapply(1:num_variables, function(i) {
+    PDPR %>%
+      filter(variable == variables[i]) %>%
+      ggplot(aes(x = rfvalue, y = value, group = ID)) +
+      stat_smooth(geom = "line", se = F, alpha = 0.5, color = "grey70") +
+      geom_smooth(
+        data = MNVL %>% filter(variable == variables[i]),
+        inherit.aes = F,
+        method = "loess",
+        aes(x = rfvalue, y = value),
+        color = colors_vec[i],
+        se = F,
+        linewidth = 1.5
+      ) +
+      labs(y = NULL, x = variables[i]) +
+      theme(
+        aspect.ratio = 1,
+        panel.background = element_rect(fill = "white", color = "grey50"),
+        panel.grid.major = element_line(color = "grey90"),
+        panel.border = element_rect(fill = "transparent", color = "black"),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.title.x = element_blank(),
+        axis.ticks.length.x = unit(0, "pt"),
+        plot.margin = margin(0, 0, 0, 0, "pt")
+      )
+  })
+  p_lab <- cowplot::ggdraw() +
+    cowplot::draw_label(
+      "Shapley Score",
+      angle = 90,
+      #fontface = "bold",
+      size = 14
+    )
+  RUGV <- train_data[, variables] %>%
+    reshape2::melt() %>%
+    rename(rfvalue = value) %>%
+    mutate(value = 0)
+  TPLT <- map(1:num_variables, function(i) {
+    RUGV %>%
+      filter(variable == variables[i]) %>%
+      ggplot() +
+      geom_density(
+        aes(x = rfvalue),
+        fill = colors_vec[i],
+        alpha = 0.4,
+        color = colors_vec[i]
+      ) +
+      scale_x_continuous(position = "bottom") +
+      scale_y_reverse(expand = expansion(mult = c(0.1, 0))) +
+      labs(x = variables[i], y = NULL) +
+      theme(
+        panel.background = element_rect(fill = "white", color = "grey50"),
+        panel.grid.major = element_line(color = "grey80"),
+        panel.grid.minor = element_line(color = "transparent"),
+        panel.border = element_rect(fill = "transparent", color = "black"),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        panel.grid.major.y = element_line(color = "transparent"),
+        aspect.ratio = 0.2,
+        plot.margin = margin(0, 0, 0, 0, "pt")
+      )
+  })
+  PTPL <- lapply(1:num_variables, function(i) PDP[[i]] / TPLT[[i]])
+  cowplot::plot_grid(
+    p_lab,
+    patchwork::wrap_plots(PTPL, ncol = round(sqrt(num_variables))),
+    nrow = 1,
+    rel_widths = c(0.05, 1),
+    rel_heights = c(1, 1)
+  ) +
+    theme(plot.background = element_rect(fill = "white", color = "transparent"))
+}
